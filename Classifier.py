f0963b6d3116c00f564bef5a52365e52f7cc1c55
maltejanssen <malte.janssen@haw-hamburg.de>
2019-03-28 11:58:17 +0100

    #29, #30, #31; add: buildChunkTree to get data into correct format for classifiers

--- Classifier.py
+++ Classifier.py
@@ -1,6 +1,7 @@
 from nltk.chunk import ChunkParserI, conlltags2tree, tree2conlltags
 import Util
 from nltk.tag import UnigramTagger, BigramTagger, TrigramTagger
+from reader.reader import ConllChunkCorpusReader
 
 class Classifier(ChunkParserI):
 
@@ -44,12 +45,16 @@ def prev_next_pos_iob(tokens, index, history):
 def bag_of_words(words):
     return dict([(word, True) for word in words])
 
+def buildChunkTree(corpusPath):
+    reader = ConllChunkCorpusReader(corpusPath, ".*", ['person', 'location', 'corporation', 'product', 'creative-work', 'group'])
+    chunkTrees = reader.chunked_sents()
+    return chunkTrees
 
 
 
 if __name__ == '__main__':
-    tagsTrain = Util.readTags(r"Data\wnut17train.conll")
-    tagsTest = Util.readTags(r"Data\emerging.test.conll")
+    tagsTrain = Util.readTags(r"Data\wnut\wnut17train.conll")
+    tagsTest = Util.readTags(r"Data\wnut\emerging.test.conll")
 
     wordTaggedSentencesTrain, entitiesTrain = Util.tokenize(tagsTrain)
     wordTaggedSentencesTest, entitiesTest = Util.tokenize(tagsTest)
@@ -67,3 +72,13 @@ if __name__ == '__main__':
     nerChunkerUnigram = Classifier(completeTaggedSentencesTrain, unigramTagger)
     eval = nerChunkerUnigram.evaluate2(completeTaggedSentencesTest)
     print(eval)
+
+    Util.writeToFile(completeTaggedSentencesTrain, r"Data\Corpus\train\train.conll")
+    Util.writeToFile(completeTaggedSentencesTest, r"Data\Corpus\test\test.conll")
+
+
+    trainChunks = buildChunkTree(r"Data\Corpus\train")
+    testChunks = buildChunkTree(r"Data\Corpus\test")
+
+    print(trainChunks)
+    print(testChunks)
