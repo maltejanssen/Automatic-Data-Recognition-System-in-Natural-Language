6a4c2bbbe77fd6f873d05939d973f80b3332c8b9
maltejanssen <malte.janssen@haw-hamburg.de>
2019-03-28 08:43:26 +0100

    #30 add bag of words and prevPosIob feature extraction functions

--- Classifier.py
+++ Classifier.py
@@ -16,6 +16,36 @@ class Classifier(ChunkParserI):
         return self.evaluate([conlltags2tree([(word, pos, entity) for (word, pos), entity in iobs]) for iobs in testSents])
 
 
+def prev_next_pos_iob(tokens, index, history):
+    word, pos = tokens[index]
+
+    if index == 0:
+        prevword, prevpos, previob = ('<START>',) * 3
+    else:
+        prevword, prevpos = tokens[index - 1]
+        previob = history[index - 1]
+
+    if index == len(tokens) - 1:
+        nextword, nextpos = ('<END>',) * 2
+    else:
+        nextword, nextpos = tokens[index + 1]
+
+    feats = {
+        'word': word,
+        'pos': pos,
+        'nextword': nextword,
+        'nextpos': nextpos,
+        'prevword': prevword,
+        'prevpos': prevpos,
+        'previob': previob
+    }
+    return feats
+
+def bag_of_words(words):
+    return dict([(word, True) for word in words])
+
+
+
 
 if __name__ == '__main__':
     tagsTrain = Util.readTags(r"Data\wnut17train.conll")
