fa7935f0a94aaede2f67cc7f5125c740a46e7d7e
maltejanssen <malte.janssen@haw-hamburg.de>
2019-06-23 21:12:17 +0200

    #96 fixed output, started implementation of fetch twitter msg

--- pytorch/heroku-app/app.py
+++ pytorch/heroku-app/app.py
@@ -1,56 +1,66 @@
 from __future__ import unicode_literals
 import sys
+import os
 import logging
 from serve import get_model_api
 from flask import Flask,render_template,url_for,request, jsonify
 from spacy import displacy
 import nltk
+import tweepy
 
 nltk.download('punkt')
 app = Flask(__name__)
 model_api = get_model_api()
-
-# # Web Scraping Pkg
-# from bs4 import BeautifulSoup
-# # from urllib.request import urlopen
-# from urllib import urlopen
-
-# # Fetch Text From Url
-# def get_text(url):
-# 	page = urlopen(url)
-# 	soup = BeautifulSoup(page)
-# 	fetched_text = ' '.join(map(lambda p:p.text,soup.find_all('p')))
-# 	return fetched_text
 
 
 
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
-def turnIntoSpacyFormat(predictions):
+def turnIntoSpacyFormat(predictions, words, text):
+    print(predictions, flush=True)
+    print(words, flush=True)
+    print(text, flush=True)
     entities = []
-    for idx, prediction in enumerate(predictions):
-        if prediction != "O":
-            if prediction[0] == "B":
-                print(idx)
+    current = 0
+    idx = 0
+    while idx < len(predictions):
+        if predictions[idx][0] != "O":
+            if predictions[idx][0] == "B" or predictions[idx][0] == "I":
                 d = {}
-                d["start"] = idx
-                d["label"] = prediction[2:]
-                d["end"] = idx
-                i = idx
-                i += 1
+                d["start"] = current
+                d["label"] = predictions[idx][2:]
+                d["end"] = current + len(words[idx])
+                current += len(words[idx])
+                if current < len(text):
+                    if text[current] == " ":
+                        current += 1
+                idx += 1
+            try:
+                next = predictions[idx]
+            except:
+                entities.append(d)
+                break
+            while next[0] == "I":
+                d["end"] = current + len(words[idx])
+                current += len(words[idx])
                 try:
-                    next = predictions[i]
+                    idx += 1
+                    next = predictions[idx]
                 except:
                     break
-                while next[0] == "I":
-                    d["end"] = i
-                    try:
-                        i += 1
-                        next = predictions[i]
-                    except:
-                        break
-                entities.append(d)
+                if current < len(text):
+                    if text[current] == " ":
+                        current += 1
+            entities.append(d)
+        try:
+            current += len(words[idx])
+        except:
+            break
+        if current < len(text):
+            if text[current] == " ":
+                current += 1
+        idx += 1
     return entities
 
 
@@ -63,23 +73,55 @@ def api():
     if request.method == "POST":
 
         input_data = request.form['rawtext']
-        output_data = model_api(input_data)
-        alignedText = output_data["words"]
-        predictions = output_data["predictions"]
+        output_data, alignedData = model_api(input_data)
+        alignedWords = alignedData["words"]
+        alignedPredictions = alignedData["predictions"]
+
+        words = output_data["words"]
+        text = output_data["text"]
+        predictions= output_data["predictions"]
+
         #response = jsonify(output_data)
-        print(predictions, flush=True)
+        #print(alignedPredictions, flush=True)
 
         if request.values.get('type') == 'image':
-            text = output_data["text"]
-            ents = turnIntoSpacyFormat((predictions))
+            ents = turnIntoSpacyFormat(predictions, words, text)
             inp = {"text": text, "ents": ents, "title": None}
             htmlm = displacy.render(inp, style="ent", manual=True)
 
-            return render_template('index.html', text=alignedText, predictions=predictions, htmlm=htmlm)
+            return render_template('index.html', text=alignedWords, predictions=alignedPredictions, htmlm=htmlm)
         else:
-            return render_template('index.html', text=alignedText, predictions=predictions)
+            return render_template('index.html', text=alignedWords, predictions=alignedPredictions)
     else:
         return render_template('index.html')
+
+
+@app.route("/fetch", methods=["GET", "POST"])
+def fetch():
+    print("here", flush=True)
+    print('OAUTH_TOKEN' in os.environ)
+    authToken = os.environ.get('OAUTH_TOKEN')
+    print(authToken, flush=True)
+    authSecret = os.environ.get('OAUTH_SECRET')
+    key = os.environ.get('CONSUMER_KEY')
+    secretKey = os.environ.get('CONSUMER_SECRET')
+
+    auth = tweepy.OAuthHandler(key, secretKey)
+    auth.set_access_token(authToken, authSecret)
+    api = tweepy.API(auth)
+
+    keyword = "football"
+    tweet = api.search(keyword, count=20)
+    for t in tweet:
+        try:
+            text = t.text
+            print(text,flush=True)
+        except:
+            continue
+        else:
+            break
+    return render_template("index.html", tweet=text)
+
 
 
 if __name__ == '__main__':
